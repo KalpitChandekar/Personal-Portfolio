f4f4f766b782fa8d22ccb33cea6c16d538c0b234
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-20 12:46:15 +0530

    add navlinks in menu secion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { close, menu } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
-  const [toggle, setToggle] = useState(!false);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav
@@ -54,6 +54,29 @@ const Navbar = () => {
             className="object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
+        </div>
+
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+            {navLinks.map((nav) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-secondary"
+                }`}
+                onClick={() => {
+                  setToggle(!toggle);
+                  setActive(nav.title);
+                }}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
