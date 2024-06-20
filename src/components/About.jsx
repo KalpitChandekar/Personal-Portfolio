ec08d21fe33f942389a794ed58a9e9eb7a341657
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-20 13:55:35 +0530

    update the font size of my intoduction

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className="mt-4 text-secondary text-[20px] max-w-3xl leading-[30px]"
       >
         I'm a frontend developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Next.js, and
