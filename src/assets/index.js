e57d8d09335b539603897b59cc59eeb83f2bbc3e
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-21 13:35:25 +0530

    import the technologies

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,23 +3,28 @@ import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
-import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
+import html from "./tech/html.png";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
+import github from "./tech/github.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
+import nextjs from "./tech/nextjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
+import sass from "./tech/sass.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import vercel from "./tech/vercel.png";
+import netlify from "./tech/netlify.png";
+import vscode from "./tech/vscode.png";
 
 import salk from "./company/salk.png";
 import salkbg from "./company/salkbg.png";
@@ -63,4 +68,9 @@ export {
   carrent,
   jobit,
   tripguide,
+  netlify,
+  nextjs,
+  vercel,
+  vscode,
+  sass,
 };
