0cf086b30fb4554b6928faa8c4028c777638b67c
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-21 20:48:03 +0530

    update projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,6 +24,9 @@ import {
   carrent,
   jobit,
   tripguide,
+  metaverse,
+  travel,
+  tictactoe,
   threejs,
   nextjs,
   sass,
@@ -171,16 +174,16 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "Metaverse",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Metaversus is a virtual reality platform allowing users to explore immersive, realistic worlds with VR devices. Users can choose different environments and track friends for a shared experience.",
     tags: [
       {
-        name: "react",
+        name: "nextjs",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "typescript",
         color: "green-text-gradient",
       },
       {
@@ -188,41 +191,43 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: carrent,
-    source_code_link: "https://github.com/",
+    image: metaverse,
+    source_code_link: "https://github.com/KalpitChandekar/Metaverse",
+    live_link: "https://metaverse-one-blond.vercel.app/",
   },
   {
-    name: "Job IT",
+    name: "Travel_UI",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "Travel-UI is a comprehensive travel app offering offline maps, augmented reality guidance, and adventure scheduling. It helps users explore new locations with ease, featuring real-time navigation and community-shared experiences.",
     tags: [
       {
         name: "nextjs",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: travel,
+    source_code_link: "https://github.com/KalpitChandekar/Travel_UI",
+    live_link: "https://travel-ui-gilt.vercel.app/",
+  },
+  {
+    name: "Tic-Tac-Toe",
+    description:
+      "Tic Tac Toe is a classic game implemented for the web, allowing players to enjoy a simple and interactive experience of the traditional X and O game against an opponent or the computer.",
+    tags: [
+      {
+        name: "javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
         color: "green-text-gradient",
       },
       {
@@ -230,8 +235,9 @@ const projects = [
         color: "pink-text-gradient",
       },
     ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
+    image: tictactoe,
+    source_code_link: "https://github.com/KalpitChandekar/Tic-Tac-Toe",
+    live_link: "https://tictactoekc.netlify.app/",
   },
 ];
 
