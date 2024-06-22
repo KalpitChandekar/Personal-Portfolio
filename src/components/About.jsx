3000d09307855e0a92da07f2c4fb5c2ce8a02fc7
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-22 16:00:31 +0530

    [fix]: update react-tilt package version

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -47,11 +47,11 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[20px] max-w-3xl leading-[30px]"
       >
-        I'm a frontend developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Next.js, and
-        Tailwind CSS. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a frontend developer with experience in TypeScript and JavaScript,
+        and expertise in frameworks like React, Next.js, and Tailwind CSS. I'm a
+        quick learner and collaborate closely with clients to create efficient,
+        scalable, and user-friendly solutions that solve real-world problems.
+        Let's work together to bring your ideas to life!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
