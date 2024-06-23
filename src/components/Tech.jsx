1e22ca472f664e4555ae1d8ad7002159d8148114
Kalpit <kalpitchandekar1736@gmail.com>
2024-06-23 10:51:54 +0530

    hide tech section for phone screen

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,7 +5,7 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    <div className="hidden sm:flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
